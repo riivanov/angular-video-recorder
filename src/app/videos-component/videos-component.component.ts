81b96bcf2015589edd8d66f9aeb815d22e1eba44
Radoslav Ivanov <riivanov63@gmail.com>
2025-04-26 15:05:43 +0300

    use promises instead of Observables

--- src/app/videos-component/videos-component.component.ts
+++ src/app/videos-component/videos-component.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Actions, Store } from '@ngxs/store';
+import { firstValueFrom } from 'rxjs';
 import { VideoStateModel } from '../../store/video/video.state';
+import { RemoveVideo } from '../../store/video/videos.actions';
 import { VideosState } from '../../store/video/videos.state';
 import { DeleteVideoDialogComponent } from '../delete-video-dialog/delete-video-dialog.component';
-import { RemoveVideo } from '../../store/video/videos.actions';
 
 @Component({
   selector: 'app-videos-component',
@@ -20,12 +21,11 @@ export class VideosComponent {
 
   constructor(private readonly store: Store) {}
 
-  removeVideo(video: VideoStateModel) {
+  async removeVideo(video: VideoStateModel) {
     const dialogRef = this.dialog.open(DeleteVideoDialogComponent);
-    dialogRef.afterClosed().subscribe((res) => {
-      if (res) {
-        this.store.dispatch(new RemoveVideo(video));
-      }
-    });
+    const res = await firstValueFrom<boolean>(dialogRef.afterClosed());
+    if (res) {
+      this.store.dispatch(new RemoveVideo(video));
+    }
   }
 }
